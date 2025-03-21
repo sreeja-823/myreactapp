9dbbbddd46fdac16b1bb2186b82dde6d3fa1b3b3
Sreeja <pasupuleti.sreeja@tredence.com>
2025-03-21 12:38:40 +0530

    added react code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Devops Starter</h1>
+        <p>This is a simple one-page UI built with React.</p>
+        <button onClick={() => alert('Button Clicked!')}>Click Me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
